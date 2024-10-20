85fc07af908c09ac6f395e6d3ac9a58450ffdbfb
Ha hieu <teamc534@gmail.com>
2024-10-20 21:38:29 +0700

    kiểm tra định dạng input

--- js/edit.js
+++ js/edit.js
@@ -1,3 +1,4 @@
+
 const form = document.querySelector("form"),
   btn = document.querySelector(".sumbit"),
   err = document.querySelector(".error_text");
@@ -9,6 +10,7 @@ btn.onclick=()=>{
     updatedata()
 }
 
+// hàm này lấy form được xử lý từ backend sau đó hiển thị ra màn hình khi load trang
 function fetch_edit() {
   const urlParams = new URLSearchParams(window.location.search);
   const userId = urlParams.get("id");
@@ -20,8 +22,20 @@ function fetch_edit() {
     });
 }
 function updatedata() {
-  let formData = new FormData(form);
-  fetch("http://localhost:8000/updateUser", { method: "POST", body: formData })
+  let formdata = new FormData(form);
+  let email = formdata.get("email"),
+  phone = formdata.get("tel"),
+  name = formdata.get("name");
+  if(!name){
+    err.innerHTML="vui lòng nhập tên";
+  }
+  else if(!validateEmail(email)||!validatePhone(phone)){
+    err.innerHTML="email hoặc phone không hợp lệ";
+ }
+ else fetch_Update(formdata);
+}
+function fetch_Update(formData){
+   fetch("http://localhost:8000/updateUser", { method: "POST", body: formData })
     .then((re) => re.text())
     .then((data) => {
       err.innerHTML = data;
@@ -30,4 +44,23 @@ function updatedata() {
     .catch((err) => {
       console.log("lỗi" + err);
     });
+}
+/* hai hàm này để kiểm tra định dạng của email và phone 
+  -- vì cái chính sách cros không cho phép lấy nguồn trực tiếp trong thư mục nên là phải copy lại file 
+  -- và để tránh mấy cái bugs suất hiện một cách vớ vẩn thì tạm thời để nó ở đây
+  -- nếu dùng server để chạy thì cros sẽ không xuất hiện.
+*/
+function validateEmail(email) {
+  if (email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+  return true;
+}
+ function validatePhone(phone) {
+  if (phone) {
+    const regex = /^(0[3|5|7|8|9])+([0-9]{8})$/;
+    return regex.test(phone);
+  }
+  return true;
 }
